896fb40ec2ba2079435b6cd555d1bb683060e9e3
Ivan Gonzalez <gonzalezbarrazaivan@gmail.com>
2021-03-27 14:00:52 -0500

    hideKeyboard. Material Theme for textview, and style applied.
    Added a ScrollView like parent to avoid truncate with button on landscape mode.

--- app/src/main/java/me/ivaangb/tipcalculator_codelab/MainActivity.kt
+++ app/src/main/java/me/ivaangb/tipcalculator_codelab/MainActivity.kt
@@ -6,10 +6,10 @@ import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import com.google.android.material.snackbar.Snackbar
 import me.ivaangb.tipcalculator_codelab.databinding.ActivityMainBinding
 import java.text.NumberFormat
-
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -19,31 +19,41 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.inputTip.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
+        binding.inputTip.setOnKeyListener { _, keyCode, _ -> handleKeyEvent(keyCode, binding.inputTip) }
 
         binding.calculateButton.setOnClickListener {
             hideKeyBoard(binding.inputTip)
+            showTip(binding.inputTip)
         }
     }
 
-    private fun showTip() {
-        val snackbar =
-            Snackbar.make(binding.calculateButton, "You must type a Cost", Snackbar.LENGTH_SHORT)
 
+    //Método que muestra el resltado
+    private fun showTip(editText: EditText) {
+        editText.clearFocus()
+        if (validateText(editText)) calculateTip(editText)
+    }
+
+    //Método que valida si el campo de texto no está vacío o no es cero
+    private fun validateText(editText: EditText): Boolean {
+        val snackbar =
+            Snackbar.make(editText, "You must type a Cost", Snackbar.LENGTH_SHORT)
         val lambda = {
             snackbar.show()
-            binding.inputTip.text = null
+            editText.text = null
         }
 
-        if (binding.inputTip.text.isNullOrEmpty() || binding.inputTip.text.toString()
-                .toDouble() == 0.0
-        ) lambda()
-        else calculateTip()
+        return if (editText.text.isNullOrEmpty() || editText.text.toString().toDouble() == 0.0){
+            lambda()
+            false
+        } else {
+            true
+        }
     }
 
-
-    private fun calculateTip() {
-        val cost = binding.inputTip.text.toString().toDouble()
+    //Método que calcula la propina y la pone en el TextView
+    private fun calculateTip(editText: EditText) {
+        val cost = editText.text.toString().toDouble()
         val selectedId = binding.tipRadioGroup.checkedRadioButtonId
 
         val tipPercentage = when (selectedId) {
@@ -56,23 +66,24 @@ class MainActivity : AppCompatActivity() {
         val roundUp = binding.roundUpSwitch.isChecked
         if (roundUp) tip = kotlin.math.ceil(tip)
 
+        //Establece el resultado en el TextView
         binding.totalTextView.text = NumberFormat.getCurrencyInstance().format(tip).toString()
-
     }
 
-    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+    //Método que esconde teclado al presionar enter.
+    private fun handleKeyEvent(keyCode: Int, editText: EditText): Boolean {
         if (keyCode == KeyEvent.KEYCODE_ENTER) {
-            hideKeyBoard(view)
-            showTip()
+            hideKeyBoard(editText)
+            showTip(editText)
             return true
         }
         return false
     }
 
+    //Método que esconde teclado
     private fun hideKeyBoard(view: View) {
         val inputMethodManager =
             getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
-        view.clearFocus()
     }
 }
